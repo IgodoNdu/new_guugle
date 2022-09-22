6d967471668237907990e2bb8f514a7f55571119
IgodoNdu <darkbluetechmail@gmail.com>
2022-09-22 15:48:56 +0100

    Update index.js
    Updating the App.js file to allow for file/page navigation/linking

--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+//for page routing/navigation
+import { BrowserRouter as Router } from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/**To easily switch/navigate to pages: the following changes */}
+    <Router>
+      <App />
+    </Router>,
   </React.StrictMode>
 );
 

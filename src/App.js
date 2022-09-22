9652f00c8f6e2041df089aeb86a17a635dd0fba4
IgodoNdu <darkbluetechmail@gmail.com>
2022-09-22 14:51:31 +0100

    Rendering the components
    Rendering the components in their respective order of display

--- src/App.js
+++ src/App.js
@@ -13,7 +13,10 @@ function App() {
     <div className={darkTheme ? 'dark' : ''}>
       {/**If in darTheme mode, make background gray strenght = 900, and text gray strength to 200 */}
       <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen">
-      Hello NEW GUUGLE
+        {/**Rendering the Navbar, Routes & Footer Components */}
+        <Navbar />
+        <Routes />
+        <Footer />
       </div>
     </div>
   );

eeb82ab848caec36ea2f8936b2b013ea981736ad
IgodoNdu <darkbluetechmail@gmail.com>
2022-09-22 14:41:24 +0100

    Dynamica styling of the layout
    Styling the layout with dynamic styles. Here I check if the dark theme mode is selected, then render a background & text strength to match with the dark mode

--- src/App.js
+++ src/App.js
@@ -9,8 +9,12 @@ function App() {
   const [darkTheme, setDarkTheme] = useState(false);
 
   return (
-    <div className="App">
+    //check if DarTheme mode is selected
+    <div className={darkTheme ? 'dark' : ''}>
+      {/**If in darTheme mode, make background gray strenght = 900, and text gray strength to 200 */}
+      <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen">
       Hello NEW GUUGLE
+      </div>
     </div>
   );
 }

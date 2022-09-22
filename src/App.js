b356a6d41623ef244c67a6411a201bee244edb50
IgodoNdu <darkbluetechmail@gmail.com>
2022-09-22 14:26:49 +0100

    tracking the state of DarkTheme via useState Hook
    Using a hook (useState) to track the value of DarkTheme

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, {useState} from "react";
 //import components
 import { Navbar } from "./components/Navbar";
 import { Routes } from "./components/Routes";
 import { Footer } from "./components/Footer";
 
 function App() {
+  //Using the useState hook to keep track of DarkTheme. We'll initialize DarkTheme to to false
+  const [darkTheme, setDarkTheme] = useState(false);
+
   return (
     <div className="App">
       Hello NEW GUUGLE

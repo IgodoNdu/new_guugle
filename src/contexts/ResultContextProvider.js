65fc3d8098bcbc30b305a3750412008bcb70ed71
IgodoNdu <darkbluetechmail@gmail.com>
2022-09-24 23:51:24 +0100

    Context for Api calls and results
    Creating a context for the api calls, this will enable us make the state available application-wide, thus no need to funnel params from parent to child components

---
+++ src/contexts/ResultContextProvider.js
@@ -0,0 +1,40 @@
+import React, {createContext, useContext, useState} from "react";
+
+const ResultContext = createContext();
+const baseUrl = 'https://google-search3.p.rapidapi.com/api/v1';
+
+export const ResultContextProvider = ({ children }) => {
+    const [results, setResults] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [searchTerm, setSearchTerm] = useState('');
+
+    //fetch results: make api calls
+    const getResults = async (type) => {
+        setIsLoading(true);
+
+        const response = await fetch(`${baseUrl}${type}`, {
+            method: 'GET',
+            headers: {
+                'X-User-Agent': 'desktop',
+                'X-Proxy-Location': 'EU',
+                'X-RapidAPI-Key': 'fd9ecad78emshb94033a3410409bp1be54bjsna39500533534',
+                'X-RapidAPI-Host': 'google-search3.p.rapidapi.com'
+            }
+        });
+
+        const data = await response.json();
+        console.log(data)
+
+        setResults(data);
+        setIsLoading(false);
+    }
+
+    return (
+        <ResultContext.Provider value={{ getResults, results, searchTerm, setSearchTerm, isLoading }} >
+            {children}
+        </ResultContext.Provider>
+    );
+}
+
+
+export const useResultContext = () => useContext(ResultContext);

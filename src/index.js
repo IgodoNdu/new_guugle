773fa9cd87adde379f597574c1866976661864df
IgodoNdu <darkbluetechmail@gmail.com>
2022-09-24 23:54:41 +0100

    Providing the Context application wide
    Providing the context to be available to all components of the application

--- src/index.js
+++ src/index.js
@@ -4,15 +4,19 @@ import App from './App';
 import './index.css';
 //for page routing/navigation
 import { BrowserRouter as Router } from 'react-router-dom';
+//import your context/state provider
+import { ResultContextProvider } from './contexts/ResultContextProvider';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/**To easily switch/navigate to pages: the following changes */}
-    <Router>
-      <App />
-    </Router>,
+    <ResultContextProvider>
+      {/**To easily switch/navigate to pages: the following changes */}
+      <Router>
+        <App />
+      </Router>
+    </ResultContextProvider>,
   </React.StrictMode>
 );
 

fa9232c94a8448527bdbf99bccf38351a6f5d558
IgodoNdu <darkbluetechmail@gmail.com>
2022-09-24 23:49:19 +0100

    Creating the loading effect component
    creating a component for the loading effect

---
+++ src/components/Loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Rings } from 'react-loader-spinner';
+
+export const Loading = () => {
+  return (
+    <div className="flex justify-center items-center ">
+        <Rings type="Puff" color="#00BFFF" height={80} width={80} radius={6} />
+    </div>
+  )
+}
+
+//export default Loading
